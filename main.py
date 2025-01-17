85221d1431c0ba87494f64ebc3b4faa4e7759c13
Kyle Greenidge <kylegreen149@gmail.com>
2025-01-17 14:56:15 -0500

    Provides basic functionality where user can draw their first two cards to start round

--- main.py
+++ main.py
@@ -14,13 +14,13 @@ However, if you get above 21, or the player after you goes above 21, then that p
       If your total is already 11 or higher, the face card's value only equals 1.
 Choose a number that corresponds with your game mode choice:
     1) Play Blackjack vs Computer
-    2) Play Blackjack vs Partner
+    2) Play Blackjack vs Player (Pass 'n Play)
     3) Exit Program
 Enter choice here: """))
         if prompt == 1:
-            print("Hi")
+            player_vs_cpu()
         elif prompt == 2:
-            print("Bye")
+            player_vs_player()
         elif prompt == 3:
             print("Closing Blackjack. Come back soon...")
             exit()
@@ -28,4 +28,25 @@ Enter choice here: """))
             print("")
             print("Only enter a number from 1-3")
             print("")
+
+def player_vs_cpu():
+    player_count = 0
+    computer_count = 0
+
+    while True:
+        cards = ["Face", 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+        users_two_cards = input("Press enter you draw your first two cards")
+        if users_two_cards == "":
+            random_index = random.randint(0, len(cards) - 1) # Draws first card
+            random_index2 = random.randint(0, len(cards) - 1) # Draws second card
+            random_card = cards[random_index] # Returns first card
+            random_card2 = cards[random_index2] # Returns second card
+            print("You drew the following cards:", random_card, random_card2)
+            player_count = random_card + random_card2
+            print("Your total is: ", player_count)
+        break
+
+def player_vs_player():
+    pass
 main_menu()
