1c826e66247705036c8d24fc86d9ba63cb168e90
Kyle Greenidge <kylegreen149@gmail.com>
2025-01-27 15:26:41 -0500

    Modified rules in main menu

--- main.py
+++ main.py
@@ -5,19 +5,18 @@ cards = ["Face", 2, 3, 4, 5, 6, 7, 8, 9, 10]
 def main_menu():
     while True:
         prompt = int(input("""Welcome to Blackjack Elite, presented to you by MiniGame Enterprizes!
-Blackjack is pretty easy to play. The goal: Try to get to 21, or as close as you can possibly get without going over it.
-Here are the rules:
-- The game starts with the first player drawing two cards. Based on the card's value, you can
-    - "Hit": Saying "Hit" means that you want to draw another card to try to get closer to 21
-    - "Stand": Saying "Stand" means that you are satisfyed with where you are, and you feel you have got close to 21 as best as you could.
-The catch: Once you say "Stand", your turn is over. It is then the next players turn, if they get closer to 21 than you do, then you lose.
-However, if you get above 21, or the player after you goes above 21, then that player loses.
-    - If you draw a face card before your total count is 11, the face card's value is equal to 11. 
-      If your total is already 11 or higher, the face card's value only equals 1.
+
+In blackjack, each player aims to beat the other by having a hand value closest to 21 without exceeding it.
+Here are the rules: 
+    - Players are dealt two cards and can choose to "hit" (take additional cards) or "stand" (keep their current hand) to achieve the best total. 
+    - Face cards are worth 11 points if the players total points are less than 11 total points, and worth 1 point if the total points are 11+ points.
+    - To win after the first player plays, the second player should have more points than the first player, but less than 21
+
 Choose a number that corresponds with your game mode choice:
     1) Play Blackjack vs Computer
     2) Play Blackjack vs Player (Pass 'n Play)
     3) Exit Program
+    
 Enter choice here: """))
         if prompt == 1:
             current_game_mode = player_vs_cpu
